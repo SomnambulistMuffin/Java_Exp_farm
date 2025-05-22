39aab319f2a3d642ac1063c4997df72bd9546221
he who likes syrup <32136015+SomnambulistMuffin@users.noreply.github.com>
2025-05-22 22:27:07 +0200

    Add files via upload

---
+++ Exam2016Pract/Exam2016Pract/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OrdersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChangeDetailsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AdminToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ManageUserTypesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ManageUsersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ManageItemsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.OrdersToolStripMenuItem, Me.AdminToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(525, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ChangeDetailsToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(40, 20)
+        Me.FileToolStripMenuItem.Text = "File "
+        '
+        'OrdersToolStripMenuItem
+        '
+        Me.OrdersToolStripMenuItem.Name = "OrdersToolStripMenuItem"
+        Me.OrdersToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.OrdersToolStripMenuItem.Text = "Orders"
+        '
+        'ChangeDetailsToolStripMenuItem
+        '
+        Me.ChangeDetailsToolStripMenuItem.Name = "ChangeDetailsToolStripMenuItem"
+        Me.ChangeDetailsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ChangeDetailsToolStripMenuItem.Text = "Change details"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "exit"
+        '
+        'AdminToolStripMenuItem
+        '
+        Me.AdminToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ManageUserTypesToolStripMenuItem, Me.ManageUsersToolStripMenuItem, Me.ManageItemsToolStripMenuItem})
+        Me.AdminToolStripMenuItem.Name = "AdminToolStripMenuItem"
+        Me.AdminToolStripMenuItem.Size = New System.Drawing.Size(55, 20)
+        Me.AdminToolStripMenuItem.Text = "Admin"
+        '
+        'ManageUserTypesToolStripMenuItem
+        '
+        Me.ManageUserTypesToolStripMenuItem.Name = "ManageUserTypesToolStripMenuItem"
+        Me.ManageUserTypesToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.ManageUserTypesToolStripMenuItem.Text = "Manage User Types"
+        '
+        'ManageUsersToolStripMenuItem
+        '
+        Me.ManageUsersToolStripMenuItem.Name = "ManageUsersToolStripMenuItem"
+        Me.ManageUsersToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.ManageUsersToolStripMenuItem.Text = "Manage Users"
+        '
+        'ManageItemsToolStripMenuItem
+        '
+        Me.ManageItemsToolStripMenuItem.Name = "ManageItemsToolStripMenuItem"
+        Me.ManageItemsToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.ManageItemsToolStripMenuItem.Text = "Manage Items"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(525, 363)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ChangeDetailsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OrdersToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AdminToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ManageUserTypesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ManageUsersToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ManageItemsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Exam2016Pract/Exam2016Pract/classUsers.vb
@@ -0,0 +1,154 @@
+﻿Imports System.Data.SqlClient
+
+
+
+
+Public Class classUsers
+
+    Private _connstring As String
+    Public Property Connstring() As String
+        Get
+            Return _connstring
+        End Get
+        Set(ByVal value As String)
+            _connstring = value
+        End Set
+    End Property
+    Private _connstringuser As String
+    Public Property Connstringuser() As String
+        Get
+            Return _connstringuser
+        End Get
+        Set(ByVal value As String)
+            _connstringuser = value
+        End Set
+    End Property
+
+    Private _DOB As String
+    Public Property DOB() As String
+        Get
+            Return _DOB
+        End Get
+        Set(ByVal value As String)
+            _DOB = value
+        End Set
+    End Property
+
+    Private _email As String
+    Public Property Email() As String
+        Get
+            Return _email
+        End Get
+        Set(ByVal value As String)
+            _email = value
+        End Set
+    End Property
+    Private _name As String
+    Public Property Name() As String
+        Get
+            Return _name
+        End Get
+        Set(ByVal value As String)
+            _name = value
+        End Set
+    End Property
+    Private _password As String
+    Public Property Password() As String
+        Get
+            Return _password
+        End Get
+        Set(ByVal value As String)
+            _password = value
+        End Set
+    End Property
+    Private _usertID As String
+    Public Property UserTypeID() As String
+        Get
+            Return _usertID
+        End Get
+        Set(ByVal value As String)
+            _usertID = value
+        End Set
+    End Property
+
+    Public Sub New()
+        Connstring = My.Settings.OrderDBConnString
+        Connstringuser = My.Settings.NUserDBConnString
+        Name = " "
+        DOB = " "
+        Email = " "
+        Password = " "
+        UserTypeID = 0
+    End Sub
+
+    Public Sub New(ByVal theemail As String)
+        Connstring = My.Settings.OrderDBConnString
+        Connstringuser = My.Settings.NUserDBConnString
+        Name = " "
+        DOB = " "
+        Email = theemail
+        Password = " "
+        UserTypeID = 0
+    End Sub
+
+    Public Sub New(ByVal theemail As String, ByVal thepassword As String, ByVal theName As String, ByVal theDOB As String)
+        Connstring = My.Settings.OrderDBConnString
+        Connstringuser = My.Settings.NUserDBConnString
+
+        Name = theName
+        DOB = theDOB
+        Email = theemail
+        Password = thepassword
+        UserTypeID = 0
+    End Sub
+
+    Public Sub GetUserdata(ByVal theEmail As String, ByVal theUserType As String, ByVal theName As String, ByVal theDOB As String, ByVal thepass As String)
+        Dim myConn As New SqlConnection(My.Settings.NUserDBConnString)
+        Dim myCommand As New SqlCommand("SELECT * FROM tblUsers WHERE Email = @paremail " & "AND Password = @parPassword," & " AND Name = @parName" & "AND DOB = @ parDOB", myConn)
+        Dim myAdapter As New SqlDataAdapter(myCommand)
+        Dim mytable As New DataTable
+        Dim objRow As DataRow
+
+        'myCommand.Parameters.Add("@paremail", SqlDbType.VarChar).Value = theEmail
+        'myCommand.Parameters.Add("@parpassword", SqlDbType.VarChar).Value = thepass
+
+        'myConn.Open()
+        'myAdapter.Fill(mytable)
+
+        'myConn.Close()
+
+        If Not IsNothing(theEmail) Then
+            myCommand.Parameters.Add("@parName", SqlDbType.VarChar).Value = theName
+            myCommand.Parameters.Add("@parDOB", SqlDbType.VarChar).Value = theDOB
+            myCommand.Parameters.Add("@parUserTypeID", SqlDbType.VarChar).Value = theUserType
+            myCommand.Parameters.Add("@parpassword", SqlDbType.VarChar).Value = thepass
+
+            myConn.Open()
+            myAdapter.Fill(mytable)
+            myConn.Close()
+
+            objRow = mytable.Rows(0)
+
+            theName = objRow("Name")
+            thepass = objRow("Password")
+            theDOB = objRow("DOB")
+
+        End If
+    End Sub
+    Public Sub UpdateUserData(ByVal theEmail As String, ByVal thePassword As String, ByVal theName As String, ByVal theDOB As String)
+        Dim myConn As New SqlConnection(My.Settings.NUserDBConnString)
+        Dim myCommand As New SqlCommand("UPDATE tblUsers SET Name = @parName," & " Password = @parPass" & ", Email = @parEmail, DOB = @parDOB WHERE Email = @parEmail", myConn)
+
+        myCommand.Parameters.Add("@parName", SqlDbType.VarChar)
+        myCommand.Parameters.Add("@parPass", SqlDbType.VarChar)
+        myCommand.Parameters.Add("@parEmail", SqlDbType.VarChar)
+        myCommand.Parameters.Add("@parDOB", SqlDbType.VarChar)
+
+        myConn.Open()
+        myCommand.ExecuteNonQuery()
+        myConn.Close()
+
+
+
+    End Sub
+End Class
