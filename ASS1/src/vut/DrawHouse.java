2f0425cecf2ef3ff3e7fed41f264e84db8da48be
he who likes syrup <32136015+SomnambulistMuffin@users.noreply.github.com>
2025-08-04 22:55:31 +0200

    Add files via upload

---
+++ ASS1/src/vut/DrawHouse.java
@@ -0,0 +1,66 @@
+package vut;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+
+/**
+ *
+ * @author st SATHIKGE_214248038
+ */
+public class DrawHouse extends Applet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    @Override
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+    
+    @Override
+    public void paint(Graphics pen) {
+        //Assign a graphic to graphics2D
+        Graphics2D pen2D = (Graphics2D) pen;
+        int x = 100;
+        int y = 100;
+        int width = 400;
+        int height = 500;
+         //final double FIFTH_HEIGHT = x/5;
+         
+        // rectangles
+        Rectangle house = new Rectangle(x, y, width, height);
+        pen2D.draw(house);
+        Rectangle garage = new Rectangle(x + width, y, width, height - (y / 2));
+        pen2D.draw(garage);
+        Rectangle car = new Rectangle(x + width + (x / 2), y * 3, width - x, y / 4);
+        pen2D.draw(car);
+        Rectangle door = new Rectangle(x * 2 + (x / 2), (y * 2) + (y / 2), width / 4, height / 2);
+        pen2D.draw(door);
+
+        //circles
+        Ellipse2D.Double myWheel1, myWheel2;
+        myWheel1 = new Ellipse2D.Double((((x * 2)) + width) - (x / 4), (y * 3) + (y / 8), x / 4, y / 4);
+        pen2D.draw(myWheel1);
+        myWheel2 = new Ellipse2D.Double(width + (x * 4), (y * 3) + (y / 8), x / 4, y / 4);
+        pen2D.draw(myWheel2);
+
+        //Lines
+        Line2D.Double carLeftLine = new Line2D.Double(width + (x * 2), y * 3, width + (x * 2) + (x / 4), (y * 3) - y / 4);
+        pen2D.draw(carLeftLine);
+        Line2D.Double carRightLine = new Line2D.Double(width + (x * 4) - (x / 5), (y * 3) - (y / 4), width + (x * 4), y * 3);
+        pen2D.draw(carRightLine);
+        Line2D.Double carLineStr8 = new Line2D.Double(width + (x * 2) + (x / 4), (y * 3) - y / 4, width + (x * 4) - (x / 5), (y * 3) - (y / 4));
+        pen2D.draw(carLineStr8);
+        Line2D.Double houseRightLine = new Line2D.Double((width + x) - (x / 2),y/2,x+width,y);
+        pen2D.draw(houseRightLine);
+        Line2D.Double HouseLeftLine = new Line2D.Double(x, y, x + (x / 2), y / 2);
+        pen2D.draw(HouseLeftLine);
+        Line2D.Double houseLineStr8 = new Line2D.Double(x + (x / 2),y/2,(width+ x)-(x/2),y/2);
+        pen2D.draw(houseLineStr8);
+        
+    }
+}
---
+++ ASS1/src/vut/houseOfShapes.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vut;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+
+/**
+ *
+ * @author SATHIKGE_214248038
+ */
+public class houseOfShapes extends Applet {
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void paint(Graphics pen) {
+        Graphics2D pen2D = (Graphics2D) pen;
+
+        int x = 250;
+        int y = 200;
+        int w = 500;
+        int h = 300;
+
+        //rectangles
+        Rectangle house, garage, door, car;
+        house = new Rectangle(x, y, w, h);
+        garage = new Rectangle(w + x, y, w - (x / 5) + (x / 5), y + (y / 4));
+        door = new Rectangle(w - (x / 4), h, w / 5, y);
+        car = new Rectangle(w + x + (w / 5), h, w - x + (x / 5), y / 4);
+
+        Line2D.Double lineHouseLeft, lineHouseRight, lineHouseStr8, lineCarLeft, lineCarRight, lineCarStr8;
+        lineHouseLeft = new Line2D.Double(x, y, (w - x) + (x / 5), y / 2);
+        lineHouseRight = new Line2D.Double(w + x, y, (w + x) - (x / 5), y / 2);
+        lineHouseStr8 = new Line2D.Double(x + (x / 5), y / 2, (w + x) - (x / 5), y / 2);
+        lineCarLeft = new Line2D.Double(w + (x * 2) - (w / 5), h, (w + x) + x - (x / 5), h - (y / 4));
+        lineCarRight = new Line2D.Double(w + w + (w / 5), h, w + w + (x / 5), h - (y / 4));
+        lineCarStr8 = new Line2D.Double((w + x) + x - (x / 5), h - (y / 4), w + w + (x / 5), h - (y / 4));
+
+        //circles
+        Ellipse2D.Double myWheel1, mywheel2;
+        myWheel1 = new Ellipse2D.Double(w + w - (w / 5), h + (y / 8), (x / 5), (y / 4));
+
+        mywheel2 = new Ellipse2D.Double(w + w + (x / 5), h + (y / 8), (x / 5), (y / 4));
+
+        pen2D.setColor(Color.MAGENTA);
+        pen2D.draw(house);
+        pen2D.fill(house);
+        pen2D.setColor(Color.YELLOW);
+        pen2D.draw(garage);
+        pen2D.fill(garage);
+        pen2D.setColor(Color.GREEN);
+        pen2D.draw(door);
+        pen2D.fill(door);
+        pen2D.setColor(Color.RED);
+        pen2D.draw(car);
+        pen2D.fill(car);
+        pen2D.setColor(Color.cyan);
+        pen2D.draw(lineHouseLeft);
+        pen2D.fill(lineHouseLeft);
+        pen2D.setColor(Color.ORANGE);
+        pen2D.draw(lineHouseRight);
+        pen2D.fill(lineHouseRight);
+        pen2D.setColor(Color.PINK);
+        pen2D.draw(lineHouseStr8);
+        pen2D.fill(lineHouseStr8);
+        pen2D.setColor(Color.blue);
+        pen2D.draw(lineCarLeft);
+        pen2D.fill(lineCarLeft);
+        pen2D.setColor(Color.darkGray);
+        pen2D.draw(lineCarRight);
+        pen2D.fill(lineCarRight);
+        pen2D.setColor(Color.GRAY);
+        pen2D.draw(lineCarStr8);
+        pen2D.fill(lineCarStr8);
+        pen2D.setColor(Color.black);
+        pen2D.draw(myWheel1);
+        pen2D.fill(myWheel1);
+        pen2D.setColor(Color.black);
+        pen2D.draw(mywheel2);
+        pen2D.fill(mywheel2);
+    }
+}
